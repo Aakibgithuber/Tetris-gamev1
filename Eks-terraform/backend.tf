f73dcc48e07a837c4c9cd64811d2e9eeaa4702a6
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2024-01-30 19:52:03 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "mario12bucket" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
