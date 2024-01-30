43b1685720636b958e4a23195b6f6468046a1c98
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2024-01-30 13:28:49 +0530

    Update install_jenkins.sh

--- Jenkins-terraform/install_jenkins.sh
+++ Jenkins-terraform/install_jenkins.sh
@@ -16,6 +16,7 @@ sudo systemctl status jenkins
 sudo apt-get update
 sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu  
+sudo usermod -aG docker jenkins
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
